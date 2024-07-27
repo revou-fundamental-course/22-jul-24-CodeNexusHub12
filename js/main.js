62f577f542a621631ada4a816419aeb328b73e28
NICO ADITYA SAPUTRA <fiola.kharisma123@gmail.com>
2024-07-28 00:53:53 +0700

    commit-3

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-// Check for saved user preference, if any, on load of the website
+
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 if (currentTheme) {
@@ -34,3 +34,24 @@ if (currentTheme) {
         toggleSwitch.checked = true;
     }
 }
+
+// JavaScript untuk mengganti gambar berdasarkan tema
+document.addEventListener('DOMContentLoaded', () => {
+    const profilImg = document.getElementById('profilKlien');
+    const theme = document.body.getAttribute('data-theme');
+
+    if (theme === 'dark') {
+        profilImg.src = 'Asset/profil-contact2.png'; 
+    } else {
+        profilImg.src = 'Asset/profil-contact.jpg'; 
+    }
+});
+
+// Menambahkan event listener untuk perubahan tema
+document.querySelector('.theme-switch input').addEventListener('change', (event) => {
+    const profilImg = document.getElementById('profilKlien');
+    const theme = event.target.checked ? 'dark' : 'light';
+
+    document.body.setAttribute('data-theme', theme);
+    profilImg.src = theme === 'dark' ? 'Asset/profil-contact2.png' : 'Asset/profil-contact.jpg';
+});
