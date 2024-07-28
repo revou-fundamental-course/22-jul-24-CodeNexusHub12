b9c2307b3aab54f3fd952cbd9632c4944ffeea0e
NICO ADITYA SAPUTRA <fiola.kharisma123@gmail.com>
2024-07-28 10:54:20 +0700

    commit-3

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
-// Theme switcher
+
 // Theme switcher
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
@@ -19,17 +18,16 @@ function switchTheme(e) {
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-    }    
+    }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
-
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-
+    
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
@@ -39,11 +37,11 @@ if (currentTheme) {
 document.addEventListener('DOMContentLoaded', () => {
     const profilImg = document.getElementById('profilKlien');
     const theme = document.body.getAttribute('data-theme');
-
+    
     if (theme === 'dark') {
-        profilImg.src = 'Asset/profil-contact2.png'; 
+        profilImg.src = 'Asset/profil-contact2.png';
     } else {
-        profilImg.src = 'Asset/profil-contact.jpg'; 
+        profilImg.src = 'Asset/profil-contact.jpg';
     }
 });
 
@@ -51,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.querySelector('.theme-switch input').addEventListener('change', (event) => {
     const profilImg = document.getElementById('profilKlien');
     const theme = event.target.checked ? 'dark' : 'light';
-
+    
     document.body.setAttribute('data-theme', theme);
     profilImg.src = theme === 'dark' ? 'Asset/profil-contact2.png' : 'Asset/profil-contact.jpg';
 });
