378164d937326e4e3d0380038edfa0ae8969119c
NICO ADITYA SAPUTRA <fiola.kharisma123@gmail.com>
2024-08-01 00:46:03 +0700

    commit-4

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ if (currentTheme) {
     }
 }
 
+
 // JavaScript untuk mengganti gambar berdasarkan tema
 document.addEventListener('DOMContentLoaded', () => {
     const profilImg = document.getElementById('profilKlien');
@@ -121,3 +122,33 @@ document.getElementById('backButton').addEventListener('click', function() {
 
     document.querySelector('form').reset();
 });
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,3000);
+  
+}
+
+changeSlide();
